0942c010c94ddb312a7ce93b8ae1a2577a08b504
franshelina camilo <franshelina_camilo@ucne.edu.do>
2026-06-03 23:05:41 -0400

    Agregar metodo de validacion de usuario

--- src/SistemaUsuario.java
+++ src/SistemaUsuario.java
@@ -8,21 +8,45 @@ public class SistemaUsuario {
     }
 
     public void registrarUsuario(Scanner sc) {
-        System.out.print("Ingrese el nombre del usuario: ");
-        String nombre = sc.nextLine();
+        try{
+            System.out.print("Ingrese el nombre del usuario: ");
+            String nombre = sc.nextLine();
 
-        System.out.print("Ingrese la edad del usuario: ");
-        int edad = sc.nextInt();
-        sc.nextLine(); 
+            System.out.print("Ingrese la edad del usuario: ");
+            int edad = sc.nextInt();
+            sc.nextLine(); 
 
-        System.out.print("Ingrese el correo electrónico del usuario: ");
-        String correoElectronico = sc.nextLine();
+            System.out.print("Ingrese el correo electrónico del usuario: ");
+            String correoElectronico = sc.nextLine();
 
-        System.out.print("Ingrese el salario mensual del usuario: ");
-        double salarioMensual = sc.nextDouble();
-        sc.nextLine();
+            System.out.print("Ingrese el salario mensual del usuario: ");
+            double salarioMensual = sc.nextDouble();
+            sc.nextLine();
 
-        Usuario nuevoUsuario = new Usuario(nombre, edad, correoElectronico, salarioMensual);
-        System.out.println("Usuario registrado exitosamente.");
+
+            Usuario nuevoUsuario = new Usuario(nombre, edad, correoElectronico, salarioMensual);
+            UsuarioService.validarUsuario(nuevoUsuario);
+
+            usuarios.add(nuevoUsuario);
+            System.out.println("Usuario registrado correctamente.");
+        } 
+        catch (NombreValidatorException e) {
+            System.out.println(e.getMessage());
+        }
+        catch (EdadValidatorException e) {
+            System.out.println(e.getMessage());
+        }
+        catch (CorreoValidatorException e) {
+            System.out.println(e.getMessage());
+        }
+        catch (SalarioValidatorException e) {
+            System.out.println(e.getMessage());
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Error: Debe ingresar un numero valido."); 
+        }
+        finally {
+            System.out.println("Proceso finalizado.");
+        }
     }
 }
--- src/UsuarioService.java
+++ src/UsuarioService.java
@@ -1,28 +1,36 @@
 public class UsuarioService {
+
+    public static void validarUsuario(Usuario usuario) throws NombreValidatorException, EdadValidatorException, CorreoValidatorException, SalarioValidatorException {
+        validarNombre(usuario.getNombre());
+        validarEdad(usuario.getEdad());
+        validarCorreoElectronico(usuario.getCorreoElectronico());
+        validarSalario(usuario.getSalarioMensual());
+    }
+
     public static void validarNombre(String nombre) throws NombreValidatorException {
         if (nombre.trim().isEmpty()) {
-            throw new NombreValidatorException("El nombre no puede estar vacío.");
+            throw new NombreValidatorException("Error: El nombre no puede estar vacío.");
         }
         if (nombre.length() < 3) {
-            throw new NombreValidatorException("El nombre debe tener al menos 3 caracteres.");
+            throw new NombreValidatorException("Error: El nombre debe contener al menos 3 caracteres.");
         }
     }
 
     public static void validarEdad(int edad) throws EdadValidatorException {
         if (edad < 18 || edad > 100) {
-            throw new EdadValidatorException("La edad debe ser mayor o igual a 18 años y menor o igual a 100 años.");
+            throw new EdadValidatorException("Error: La edad debe ser mayor o igual a 18 años.");
         }
     }
 
     public static void validarCorreoElectronico(String correo) throws CorreoValidatorException {
         if (!correo.contains("@") || !correo.contains(".") ) {
-            throw new CorreoValidatorException("El correo electrónico no es válido.");
+            throw new CorreoValidatorException("Error: El correo electrónico no es válido.");
         }
     }
 
     public static void validarSalario(double salario) throws SalarioValidatorException {
         if (salario < 0) {
-            throw new SalarioValidatorException("El salario no puede ser negativo.");
+            throw new SalarioValidatorException("Error: El salario debe ser mayor que 0.");
         }
     }
 }
